972f2ed4cd9f00a7a2b7ecb9e2587de5c3b7fd03
rlarjsgml <rlarjsgml0701@tukorea.ac.kr>
2023-07-18 21:41:47 +0900

    rpi update

--- rpi.py
+++ rpi.py
@@ -34,68 +34,68 @@ PWM2 = 36
 
 GPIO.setwarnings(False)
 # 리니어 모터
-GPIO.setmode(GPIO.BOARD)                # 핀번호 설정을 BOARD 방식으로 지정
-GPIO.setup(Dir_Linear, GPIO.OUT)        # 방향 설정 핀을 출력 핀으로 설정
-GPIO.setup(SPD, GPIO.OUT)               # 속도 제어 핀을 출력 핀으로 설정
-GPIO.setup(BRK, GPIO.OUT)               # 브레이크 제어 핀을 출력 핀으로 설정
+GPIO.setmode(GPIO.BOARD)                    # 핀번호 설정을 BOARD 방식으로 지정
+GPIO.setup(Dir_Linear, GPIO.OUT)            # 방향 설정 핀을 출력 핀으로 설정
+GPIO.setup(SPD, GPIO.OUT)                   # 속도 제어 핀을 출력 핀으로 설정
+GPIO.setup(BRK, GPIO.OUT)                   # 브레이크 제어 핀을 출력 핀으로 설정
 
 # hc-sr04
 GPIO.setup(trig, GPIO.OUT)
 GPIO.setup(echo, GPIO.IN)
 
 #DC 모터
-GPIO.setup(IN1, GPIO.OUT)               # IN1 핀을 출력 핀으로 설정
-GPIO.setup(IN2, GPIO.OUT)               # IN2 핀을 출력 핀으로 설정
-GPIO.setup(PWM1, GPIO.OUT)              # PWM1 핀을 출력 핀으로 설정
+GPIO.setup(IN1, GPIO.OUT)                   # IN1 핀을 출력 핀으로 설정
+GPIO.setup(IN2, GPIO.OUT)                   # IN2 핀을 출력 핀으로 설정
+GPIO.setup(PWM1, GPIO.OUT)                  # PWM1 핀을 출력 핀으로 설정
 
-GPIO.setup(IN3, GPIO.OUT)               # IN3 핀을 출력 핀으로 설정
-GPIO.setup(IN4, GPIO.OUT)               # IN4 핀을 출력 핀으로 설정
-GPIO.setup(PWM2, GPIO.OUT)              # PWM2 핀을 출력 핀으로 설정
+GPIO.setup(IN3, GPIO.OUT)                   # IN3 핀을 출력 핀으로 설정
+GPIO.setup(IN4, GPIO.OUT)                   # IN4 핀을 출력 핀으로 설정
+GPIO.setup(PWM2, GPIO.OUT)                  # PWM2 핀을 출력 핀으로 설정
 
-pwm1 = GPIO.PWM(PWM1, 490)              # PWM1 설정
-pwm2 = GPIO.PWM(PWM2, 490)              # PWM2 설정
+pwm1 = GPIO.PWM(PWM1, 490)                  # PWM1 설정
+pwm2 = GPIO.PWM(PWM2, 490)                  # PWM2 설정
 pwm1.start(0)
 pwm2.start(0)
 
-GPIO.output(IN1, GPIO.HIGH)             # IN1 핀 출력 설정
-GPIO.output(IN2, GPIO.LOW)              # IN2 핀 출력 설정
-GPIO.output(IN3, GPIO.HIGH)             # IN3 핀 출력 설정
-GPIO.output(IN4, GPIO.LOW)              # IN4 핀 출력 설정
+GPIO.output(IN1, GPIO.HIGH)                 # IN1 핀 출력 설정
+GPIO.output(IN2, GPIO.LOW)                  # IN2 핀 출력 설정
+GPIO.output(IN3, GPIO.HIGH)                 # IN3 핀 출력 설정
+GPIO.output(IN4, GPIO.LOW)                  # IN4 핀 출력 설정
 
 def move_forward(speed1, speed2):
-    GPIO.output(IN1, GPIO.HIGH)         # IN1 핀 출력 설정
-    GPIO.output(IN2, GPIO.LOW)          # IN2 핀 출력 설정
-    GPIO.output(IN3, GPIO.HIGH)         # IN3 핀 출력 설정
-    GPIO.output(IN4, GPIO.LOW)          # IN4 핀 출력 설정
+    GPIO.output(IN1, GPIO.HIGH)             # IN1 핀 출력 설정
+    GPIO.output(IN2, GPIO.LOW)              # IN2 핀 출력 설정
+    GPIO.output(IN3, GPIO.HIGH)             # IN3 핀 출력 설정
+    GPIO.output(IN4, GPIO.LOW)              # IN4 핀 출력 설정
     
     pwm1.ChangeDutyCycle(speed1)
     pwm2.ChangeDutyCycle(speed2)
     
 def move_backward(speed1, speed2):
-    GPIO.output(IN1, GPIO.LOW)          # IN1 핀 출력 설정
-    GPIO.output(IN2, GPIO.HIGH)         # IN2 핀 출력 설정
-    GPIO.output(IN3, GPIO.LOW)          # IN3 핀 출력 설정
-    GPIO.output(IN4, GPIO.HIGH)         # IN4 핀 출력 설정
+    GPIO.output(IN1, GPIO.LOW)              # IN1 핀 출력 설정
+    GPIO.output(IN2, GPIO.HIGH)             # IN2 핀 출력 설정
+    GPIO.output(IN3, GPIO.LOW)              # IN3 핀 출력 설정
+    GPIO.output(IN4, GPIO.HIGH)             # IN4 핀 출력 설정
     
     pwm1.ChangeDutyCycle(speed1)
     pwm2.ChangeDutyCycle(speed2)
     
 def stop():
-    GPIO.output(IN1, GPIO.LOW)          # IN1 핀 출력 설정
-    GPIO.output(IN2, GPIO.LOW)          # IN2 핀 출력 설정
-    GPIO.output(IN3, GPIO.LOW)          # IN3 핀 출력 설정
-    GPIO.output(IN4, GPIO.LOW)          # IN4 핀 출력 설정
+    GPIO.output(IN1, GPIO.LOW)              # IN1 핀 출력 설정
+    GPIO.output(IN2, GPIO.LOW)              # IN2 핀 출력 설정
+    GPIO.output(IN3, GPIO.LOW)              # IN3 핀 출력 설정
+    GPIO.output(IN4, GPIO.LOW)              # IN4 핀 출력 설정
     pwm1.ChangeDutyCycle(0)
     pwm2.ChangeDutyCycle(0)
     
 def linear_forward(speed):
-    GPIO.output(Dir_Linear, GPIO.HIGH)  # 전진 방향 설정
-    GPIO.output(BRK, GPIO.LOW)          # 브레이크 해제
-    pwm = GPIO.PWM(SPD, 100)            # PWM 설정
-    pwm.start(speed)                    # 속도 설정
-    time.sleep(8)                       # 2초 동안 구동
-    pwm.stop()                          # PWM 정지
-    GPIO.output(BRK, GPIO.HIGH)         # 브레이크 설정
+    GPIO.output(Dir_Linear, GPIO.HIGH)      # 전진 방향 설정
+    GPIO.output(BRK, GPIO.LOW)              # 브레이크 해제
+    pwm = GPIO.PWM(SPD, 100)                # PWM 설정
+    pwm.start(speed)                        # 속도 설정
+    time.sleep(8)                           # 2초 동안 구동
+    pwm.stop()                              # PWM 정지
+    GPIO.output(BRK, GPIO.HIGH)             # 브레이크 설정
     
 # HC-SR04 초음파 센서 : 센서값 읽기
 def calculate_distance():
